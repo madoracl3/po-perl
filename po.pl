84913ff59e4e76753155f4dc6c1a738760238008
madoracl3 <43884382+madoracl3@users.noreply.github.com>
2018-10-05 07:56:18 -0600

    Initial Upload

---
+++ po.pl
@@ -0,0 +1,24 @@
+#!/export/home/sras/autodx/common/qe/perl5/bin/perl
+# PartOverride Parser by Tony Haddaway and Don Armstead
+# v1.0 10/3/18
+
+use strict ;
+use warnings ;
+
+my $count = 0 ;
+my $result ;
+my $word = "<result>" ;
+
+$result = system("java -jar /autodx/prevalent_services/PartOverrideLookup.jar $ARGV[0]") ;
+$count = () = $result = tr/$word/g/ ;
+
+print "$count\n" ;
+
+if ($count == 0) {
+    print "Error. No part numbers found.\n" ;
+} elsif ($count == 1) {
+    print "Single part number found.\n" ;
+} elsif ($count > 1) {
+    print "Multiple part numbers.\n" ;
+}
+
